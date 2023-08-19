c4455cc6d3872cf263a6398937b40cbd3b3b6fd9
holmesfems <holmesfems@gmail.com>
2023-08-19 09:05:39 +0900

    test

--- RiseiCalculator.py
+++ RiseiCalculator.py
@@ -216,7 +216,7 @@ class RecruitView(discord.ui.View):
         if(selectedList):
             await inter.response.defer(thinking=True)
             msg = recruitDoProcess(selectedList,minstar)
-            await inter.followup.send("")
+            await inter.followup.fetch()
             await replyToDiscord(self.parent,msg)
         else:
             await inter.response.defer()
