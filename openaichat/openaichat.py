64fbb2996bb3a631cf588ce44af143a7b8a407fd
holmesfems <holmesfems@gmail.com>
2023-08-26 17:06:28 +0900

    fix

--- openaichat/openaichat.py
+++ openaichat/openaichat.py
@@ -1,16 +1,18 @@
 
 import openai,os
 
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
 
 with open("openaichat/systemPrompt.txt","r",encoding="utf-8_sig") as f:
     SYSTEM_PROMPT = f.read()
 
 #print(SYSTEM_PROMPT)
 
-openai.api_key = OPENAI_API_KEY
+
 
 def openaichat(msgList):
+    OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+    openai.api_key = OPENAI_API_KEY
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
