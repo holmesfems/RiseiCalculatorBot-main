efc4a6752de3fe46fd76bd16e524d9797fb653ef
holmesfems <holmesfems@gmail.com>
2023-09-10 09:46:51 +0900

    fix bug

--- RiseiCalculator.py
+++ RiseiCalculator.py
@@ -72,7 +72,7 @@ def extractFileAndMsg(msg):
 async def replyToDiscord(inter:Interaction,msg):
     embeds = createEmbedList(msg)
     await inter.followup.send(embeds = embeds)
-    (msg,file) = extractFileAndMsg(msg)
+    msg,file = extractFileAndMsg(msg)
     if(msg):
         await inter.followup.send(content=msg,file=file)
     #await inter.followup
