5244a9678d7309d07dd15cc9ac0b210e63fd7823
holmesfems <holmesfems@gmail.com>
2023-10-15 12:20:38 +0900

    change requests to aiohttps

--- rcutils/netutil.py
+++ rcutils/netutil.py
@@ -1,5 +1,3 @@
-import urllib.request
-import yaml
 from typing import Dict,List,Any
 import aiohttp
 import asyncio
@@ -9,23 +7,6 @@ def getUrlWithReq(url:str,AdditionalReq:Dict[str,str]=None) -> str:
     if AdditionalReq is not None:
         url += "?" + "&".join(['%s=%s'%(key,value) for key,value in AdditionalReq.items()])
     return url
-
-def get_json(url:str,AdditionalReq:Dict[str,str]=None,headers = {}) -> Any:
-    url = getUrlWithReq(url,AdditionalReq)
-    print("request:"+url)
-    Err = None
-    for i in range(10):
-        try:
-            req = urllib.request.Request(url, None,headers)
-            with urllib.request.urlopen(req, timeout=10) as response: #謎に一回目だけTimeout なぜ
-                #print(response)
-                ret = yaml.safe_load(response.read().decode())
-                print("recieved:"+url)
-                return ret
-        except Exception as e:
-            Err = e
-            print(f"failed (epoch: {i+1})")
-    raise Err
 
 def get_json_aio(urlList:List[str],headers = {}) -> tuple:
     async def get_json_single(session:aiohttp.ClientSession, url:str):
@@ -42,5 +23,8 @@ def get_json_aio(urlList:List[str],headers = {}) -> tuple:
         async with aiohttp.ClientSession(headers=headers,timeout =timeout) as session:
             tasks = [asyncio.ensure_future(get_json_single(session,url)) for url in urlList]
             return await asyncio.gather(*tasks)
-        
     return tuple(asyncio.run(mainProcess()))
+
+def get_json(url:str,AdditionalReq:Dict[str,str]=None,headers = {}) -> Any:
+    url = getUrlWithReq(url,AdditionalReq)
+    return get_json_aio([url],headers)[0]
