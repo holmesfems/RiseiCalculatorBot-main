c8d43ab7e35195cf765a5f9788ffc907f32649a9
holmesfems <holmesfems@gmail.com>
2023-11-07 09:18:57 +0900

    change model name

--- openaichat/openaichat.py
+++ openaichat/openaichat.py
@@ -86,7 +86,7 @@ def openaichat(msgList) -> ChatReply:
     with open("openaichat/systemPrompt.txt","r",encoding="utf-8_sig") as f:
         SYSTEM_PROMPT = f.read()
     response = openai.ChatCompletion.create(
-        model="gpt-4",
+        model="gpt-4-1106-preview",
         messages=[
             {"role": "system", "content" : SYSTEM_PROMPT},
         ] + msgList,
