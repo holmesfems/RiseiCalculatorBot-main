629b3fa3ad817fce01af963d6f2fbb77af0894e2
holmesfems <holmesfems@gmail.com>
2024-01-13 22:17:44 +0900

    課金パック情報更新

--- riseicalculator2/constRiseiValues.py
+++ riseicalculator2/constRiseiValues.py
@@ -11,16 +11,21 @@ valueDict = {
     "初級SoC":18*(1-12*LMDVALUE_1000/1000),
     "中級SoC":36*(1-12*LMDVALUE_1000/1000),
     "SoC強化剤":90*(30*(1-12*LMDVALUE_1000/1000)/21),
+    "特級素材交換券": 90,
+    "高級素材交換券": 26,
 }
 valueDict["上級SoC"] = 2*valueDict["中級SoC"] + valueDict["SoC強化剤"]
 valueDict["モジュールデータ"]=120*(30*(1-12*LMDVALUE_1000/1000)/21)
 valueDict["純正源石"] = 135
 valueDict["コーデ交換券"] = valueDict["純正源石"]*18
-valueDict["中級SoCセルフプリンター"] = valueDict["中級SoC"]
+valueDict["中級SoCセルフプリンター"] = valueDict["中級SoC"]*8 + valueDict["初級SoC"]*5
 valueDict["★6印交換券"] = valueDict["スカウト券"]*69.2
 valueDict["10回スカウト券"] = valueDict["スカウト券"]*10
 valueDict["エリート特訓装置"] = EXPVALUE_1000*495 + LMDVALUE_1000*447.378
 valueDict["上級エリート特訓装置"] = EXPVALUE_1000*750 + LMDVALUE_1000*744.955
+valueDict["テンニンカ指名契約"] = valueDict["スカウト券"]
+valueDict["★5特訓招待状"] = 371.947*LMDVALUE_1000 + 239.4*EXPVALUE_1000 + 4*valueDict["初級SoC"] + 3*valueDict["上級SoC"] + 1100
+valueDict["★6特訓招待状"] = 589.841*LMDVALUE_1000 + 361.4*EXPVALUE_1000 + 5*valueDict["初級SoC"] + 4*valueDict["上級SoC"] + 1700
 
 print(valueDict)
 
