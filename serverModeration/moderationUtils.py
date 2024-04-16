365e96c64740c428266e38d439069bd481c6f402
holmesfems <holmesfems@gmail.com>
2024-04-17 08:53:22 +0900

    BANの条件を改善

--- serverModeration/moderationUtils.py
+++ serverModeration/moderationUtils.py
@@ -9,17 +9,34 @@ class serverModerator:
 
     async def moderingMSG(self,message:discord.Message):
         #指定したチャンネルは、追加したメッセージをすべて削除
-        doneAny = False
-        doneAny |= await self.autoBan(message)
-        doneAny |= await self.autoDeletion(message)
+        doneAny = await self.autoDeletion(message)
+        if(not doneAny): doneAny = await self.autoBan(message)
         return doneAny
 
     async def autoDeletion(self,message:discord.Message) -> bool:
         TARGET_CHANNNEL_IDS = [
-            int(os.environ["AUTODEL_1"])
+            int(os.environ["AUTODEL_1"]),
+            int(os.environ["AUTODEL_2"]),
+            int(os.environ["AUTODEL_3"]),
+            int(os.environ["AUTODEL_4"]),
+            int(os.environ["AUTODEL_5"]),
+            int(os.environ["AUTODEL_6"])
         ]
         if message.channel.id in TARGET_CHANNNEL_IDS:
             await message.delete()
+            await self.autoBan_inAutoDeletion(message)
+            return True
+        return False
+    
+    async def autoBan_inAutoDeletion(self,message:discord.Message) -> bool:
+        #しゃべるべきではないチャンネルでしゃべると、緩い条件でBAN
+        BANWORDS = [
+            "discord.gg",
+            "http"
+        ]
+        if any(word in message.content for word in BANWORDS):
+            await message.author.ban(delete_message_days=7)
+            await self.createReport("スパムメッセージを検出したので、自動BANを実行しました。",message)
             return True
         return False
 
@@ -33,6 +50,8 @@ class serverModerator:
             await self.createReport("スパムメッセージを検出したので、自動BANを実行しました。",message)
             return True
         return False
+    
+
 
     async def createReport(self,report:str, message:discord.Message) -> None:
         content = f"{report}\n"
