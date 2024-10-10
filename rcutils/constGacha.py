a43815362139604597ed0908c4c04cbe6af531b2
holmesfems <holmesfems@gmail.com>
2024-10-10 20:32:51 +0900

    課金パック更新

--- rcutils/constGacha.py
+++ rcutils/constGacha.py
@@ -6,6 +6,8 @@ gachaDict["合成玉"] = 1/600
 gachaDict["純正源石"] = 180/600
 gachaDict["スカウト券"] = 1
 gachaDict["10回スカウト券"] = 10
+gachaDict["イベントスカウト券"] = 1
+gachaDict["イベント10回スカウト券"] = 10
 
 with open("rcutils/constGacha.yaml","wb") as f:
     yaml.safe_dump(gachaDict,f,allow_unicode=True,encoding="utf-8",sort_keys=False)
