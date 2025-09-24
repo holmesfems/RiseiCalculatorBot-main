21a27a541c0d4d02a67dd7a557538c4e7890e2d5
holmesfems <holmesfems@gmail.com>
2025-09-24 20:25:00 +0900

    ポート変更

--- RiseiCalculator.py
+++ RiseiCalculator.py
@@ -681,7 +681,7 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
         path = parsed.path.replace('/','')
         self.wfile.write(f'{executeHTTPFunction(path,params)}'.encode())
 
-server_address = ('0.0.0.0',443)
+server_address = ('0.0.0.0',80)
 
 def startServer():
     httpd = HTTPServer(server_address,CustomHTTPRequestHandler)
