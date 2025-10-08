9b144238eaa58595356fb4c25da83d1cb4955ae6
holmesfems <holmesfems@gmail.com>
2025-10-08 10:46:12 +0900

    return結果をmodel形式に

--- httpserver.py
+++ httpserver.py
@@ -34,11 +34,11 @@ class TagReplyData(BaseModel):
 def doRecruitment(data:OCRRawData):
     text= data.text
     matchTag = recruitFromOCR.matchTag(text)
-    if(matchTag.isEmpty()): return {"reply":"タグがありません","title":"エラー"}
+    if(matchTag.isEmpty()): return TagReplyData(title="エラー",reply="タグがありません")
     matches = matchTag.matches
     if(len(matches) > 8):
         matches = list(matches)[:8]
     reply = recruitment.recruitDoProcess(matches,4,matchTag.isGlobal)
-    return {"reply":reply.responseForAI, "title":reply.embbedTitle}
+    return TagReplyData(title=reply.embbedTitle,reply=reply.responseForAI)
 
 print("Server started")
