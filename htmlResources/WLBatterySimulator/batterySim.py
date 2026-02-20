e564276bc64fd17f0980d08d1d61264defd7bdfb
holmesfems <holmesfems@gmail.com>
2026-02-20 09:00:44 +0900

    シミュレート範囲調整

--- htmlResources/WLBatterySimulator/batterySim.py
+++ htmlResources/WLBatterySimulator/batterySim.py
@@ -148,7 +148,7 @@ def simulate(requiredPower:int, controller:PowerControllerWuling):
         return True
 
     #二周期分シミュレートする
-    for i in range(2*period+1):
+    for i in range(2*period):
         if( not doOnce()):
             return BatterySimResult(time=t,value=v,isValid=False)
     return BatterySimResult(time=t,value=v,isValid=True)
